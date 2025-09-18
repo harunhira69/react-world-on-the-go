538c9e3dd1d2cf76443be559fbfcdda9e0619bb7
harunhira69 <harunabhi4@gmail.com>
2025-09-19 03:09:26 +0600

    added visited card

--- src/Component/Countries/Countries.jsx
+++ src/Component/Countries/Countries.jsx
@@ -1,15 +1,27 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countryData=use(countriesPromise)
   const countries = countryData.countries;
-  console.log(countries)
+  const[visitesCountries,setVisitedCountries] = useState([])
+const handleVisitedCountries=(country)=>{
+    const newVisitedCountry =[...visitesCountries,country];
+    setVisitedCountries(newVisitedCountry)
+
+  }
+ 
     return (
-        <div className='countries'>
+        <div >
             <h3>In The Countries:{countries.length}</h3>
-            {countries.map(country=><Country key={country.cca3.cca3} country={country}></Country>)}
+            <h4>Total Country Visited:{visitesCountries.length}</h4>
+           <div className='countries'>
+             {countries.map(country=><Country 
+             key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries} >
+
+             </Country>)}
+           </div>
         </div>
     );
 };
--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-    console.log(country)
+
+const Country = ({country,handleVisitedCountries}) => {
+ 
+  const[visited,setVisited] = useState(false)
+  const handleVisited=()=>{
+setVisited(visited?false:true)
+handleVisitedCountries(country)
+  
+  }
     return (
-        <div className='country'>
+        <div className={`country ${visited&&'country-visited'}`}>
           <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
           <h3>Name:{country.name.common}</h3>
           <p>Population:{country.population.population}</p>
           <h3>Region:{country.region.region}</h3>
           <h3>Capital:{country.capital.capital}</h3>
           <h3>Continents:{country.continents.continents}</h3>
+          <h4>Area:{country.area.area}{country.area.area>300000?'Big Country':'Small Country'}</h4>
+          <button  onClick={handleVisited}>
+            {visited?'Visited':'Not Visited'}
+          </button>
          
 
         </div>
